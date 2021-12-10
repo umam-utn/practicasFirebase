624956278bdbf829c3bf46425cb2d5e6b58c0b8e
Mike <74019725+umam-utn@users.noreply.github.com>
2021-12-09 22:28:47 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyC-FMdE-FkwIDV6lECUwMjzpLA_mvK8ss4",
+  authDomain: "umam-utn-1001.firebaseapp.com",
+  projectId: "umam-utn-1001",
+  storageBucket: "umam-utn-1001.appspot.com",
+  messagingSenderId: "58276577639",
+  appId: "1:58276577639:web:d4413e76bbb27b690bafc2"
+    });
